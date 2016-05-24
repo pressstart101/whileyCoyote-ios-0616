fcd03c68c5ddb7fafcbfea0b8000addccc470f18
pressstart101 <walterscott404@yandex.com>
2016-05-24 17:53:55 -0400

    done

--- WhileyCoyote/FISAppDelegate.m
+++ WhileyCoyote/FISAppDelegate.m
@@ -5,6 +5,20 @@
 @implementation FISAppDelegate
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     
+    
+    NSUInteger steps = 0;
+    NSUInteger anvil = arc4random_uniform(25) + 26;
+    do {
+        if (steps % 10 && steps > 0){
+            NSLog(@"YOU'RE CUCKOO!");
+        } if(steps == anvil){
+            NSLog(@"SMASH!");
+        }
+        NSLog(@"%lu Meep!Meep!", steps + 1);
+        steps++;
+    } while (steps <= anvil);
+    
+    
     /**
      
      * Write your loop here!
